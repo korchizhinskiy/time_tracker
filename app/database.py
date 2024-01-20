e5a47878a24ad0106ce0cd73c2203d920aa0d957
korchizhinskiy <korchizhinskiy@gmail.com>
2024-01-20 22:48:03 +0700

    chore: change database settings

--- app/database.py
+++ app/database.py
@@ -1,20 +1,13 @@
-from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 
-from app.celery import settings
+from app.config import get_settings
 
-if settings.MODE == "TEST":
-    DATABASE_URL = (
-        f"postgresql+asyncpg://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@"
-        f"{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
-    )
-    DATABASE_PARAMS = {"poolclass": NullPool}
-else:
-    DATABASE_URL = (
-        f"postgresql+asyncpg://{settings.TEST_DATABASE_USER}:{settings.TEST_DATABASE_PASSWORD}@"
-        f"{settings.TEST_DATABASE_HOST}:{settings.TEST_DATABASE_PORT}/{settings.TEST_DATABASE_NAME}"
-    )
-    DATABASE_PARAMS = {}
+settings = get_settings()
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@"
+    f"{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+)
+DATABASE_PARAMS = {}
 
 engine: AsyncEngine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 async_session_maker = async_sessionmaker(
