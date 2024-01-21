45d0b577b756d0e662b1b60638be7bce8efadb6a
korchizhinskiy <korchizhinskiy@gmail.com>
2024-01-21 15:09:14 +0700

    feat: add base class into database module

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from app.config import get_settings
 
@@ -17,3 +18,7 @@ async_session_maker = async_sessionmaker(
     autoflush=False,
     autocommmit=False,
 )
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
