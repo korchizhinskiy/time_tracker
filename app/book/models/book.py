b17cfd9dc6952a54660062c74f8f36b8afa0a481
korchizhinskiy <korchizhinskiy@gmail.com>
2024-01-22 22:54:59 +0700

    feat: remove hybrid property

--- app/book/models/book.py
+++ app/book/models/book.py
@@ -2,8 +2,7 @@ from typing import TYPE_CHECKING
 from uuid import UUID
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import InstrumentedAttribute, Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from uuid_extensions import uuid7
 
 from app.database import Base
@@ -24,12 +23,3 @@ class Book(Base):
 
     category_id: Mapped[UUID] = mapped_column(ForeignKey("book_categories.id"))
     category: Mapped["BookCategory"] = relationship(back_populates="books")
-
-    @hybrid_property
-    def title_and_publish_year(self) -> str:
-        return f"{self.title} -> {self.publish_year}"
-
-    @title_and_publish_year.expression
-    @classmethod
-    def _title_and_publish_year_expression(cls) -> InstrumentedAttribute[str]:
-        return cls.title
