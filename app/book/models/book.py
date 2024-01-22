a3ae6a242003acd6ed1fd359524c45894020791f
korchizhinskiy <korchizhinskiy@gmail.com>
2024-01-22 22:53:15 +0700

    feat: rename book table, add hybrid property

--- app/book/models/book.py
+++ app/book/models/book.py
@@ -1,7 +1,9 @@
 from typing import TYPE_CHECKING
 from uuid import UUID
 
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import InstrumentedAttribute, Mapped, mapped_column, relationship
 from uuid_extensions import uuid7
 
 from app.database import Base
@@ -11,13 +13,23 @@ if TYPE_CHECKING:
 
 
 class Book(Base):
-    __tablename__ = "book"
+    __tablename__ = "books"
 
-    uuid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid7)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid7)
 
     title: Mapped[str]
     publish_year: Mapped[int]
     page_count: Mapped[int]
     weight: Mapped[int | None]
 
+    category_id: Mapped[UUID] = mapped_column(ForeignKey("book_categories.id"))
     category: Mapped["BookCategory"] = relationship(back_populates="books")
+
+    @hybrid_property
+    def title_and_publish_year(self) -> str:
+        return f"{self.title} -> {self.publish_year}"
+
+    @title_and_publish_year.expression
+    @classmethod
+    def _title_and_publish_year_expression(cls) -> InstrumentedAttribute[str]:
+        return cls.title
