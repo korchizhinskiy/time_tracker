f1cd56155f446bed9608d494e8ccca10a7a691ab
korchizhinskiy <korchizhinskiy@gmail.com>
2024-02-17 21:19:35 +0700

    refactor: delete models package

--- app/models/base.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-from pydantic import BeforeValidator
-from pydantic.fields import Field
-from pydantic.main import BaseModel
-
-
-class BaseMongoModel(BaseModel):
-    id: Annotated[str, BeforeValidator(str), Field(alias="_id")]
