f8645c8d945176b28b968b04fb873c76353675d1
korchizhinskiy <korchizhinskiy@gmail.com>
2024-02-17 21:21:56 +0700

    feat: include the employee's router

--- app/main.py
+++ app/main.py
@@ -1,29 +1,12 @@
 from logging.config import dictConfig
-from time import perf_counter
 
-from fastapi import Depends, FastAPI
-from motor.core import AgnosticClientSession, AgnosticDatabase
-from pydantic import BaseModel
-from app.dependencies import get_database, get_session
+from fastapi import FastAPI
 
 from app.logging import LOGGING
+from app.routers.employee import router as employee_router
 
 app = FastAPI()
+app.include_router(employee_router)
 
-class MongoModel(BaseModel):
-    name: str
-    
-    
 # Set logging settings.
 dictConfig(LOGGING)
-
-
-@app.get("/test")
-async def test(
-    session: AgnosticClientSession = Depends(get_session),
-    database: AgnosticDatabase = Depends(get_database),
-):
-    async with session as session:
-        cursor = database.messages.find({})
-        return [MongoModel(**document) for document in await cursor.to_list(length=None)]
-        
